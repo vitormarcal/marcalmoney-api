cefe98451bfa349e3827bc7287849f8aae9c0a44
Vítor Marçal <vitor.marcal@altran-ais.com.br>
2017-07-18 10:15:48 -0300

    primeiro commit

---
+++ src/main/java/com/vitormarcal/marcalmoney/api/MarcalmoneyApiApplication.java
@@ -0,0 +1,12 @@
+package com.vitormarcal.marcalmoney.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcalmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcalmoneyApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vitormarcal/marcalmoney/api/MarcalmoneyApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vitormarcal.marcalmoney.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarcalmoneyApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
