294534bc2c3fc3ee527f11c0c465b6b5159ec0b6
Vítor Marçal <vitor.marcal@altran-ais.com.br>
2017-07-26 12:41:22 -0300

    refinado a criação de categorias e implementado serviço para consulta de categorias

--- src/main/java/com/vitormarcal/marcalmoney/api/resource/CategoriaResource.java
+++ src/main/java/com/vitormarcal/marcalmoney/api/resource/CategoriaResource.java
@@ -1,15 +1,21 @@
 package com.vitormarcal.marcalmoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vitormarcal.marcalmoney.api.model.Categoria;
 import com.vitormarcal.marcalmoney.api.repository.CategoriaRepository;
@@ -27,9 +33,16 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public Categoria criar(@RequestBody Categoria categoria){
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response){
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
-		return categoriaSalva;
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+			.buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		return ResponseEntity.created(uri).body(categoria);
+	}
+	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo){
+		return categoriaRepository.findOne(codigo);
 	}
 }
