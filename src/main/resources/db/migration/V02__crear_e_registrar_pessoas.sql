c48ddecdadf7dbcbf072dd25d1ee16fd4ce48582
vitormarcal <1Qazxsw2>
2017-08-16 13:29:58 -0300

    refactor: adicionado inserts a migration

--- src/main/resources/db/migration/V02__crear_e_registrar_pessoas.sql
+++ src/main/resources/db/migration/V02__crear_e_registrar_pessoas.sql
@@ -10,3 +10,17 @@ CREATE TABLE pessoa (
   cidade VARCHAR(50)  NOT NULL,
   estado VARCHAR(50)  NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO pessoa (nome, ativo, numero, cep, cidade, estado, logradouro, bairro, complemento)
+  values ('Vítor', 1, '3', '7226000', 'ceilandia', 'df', 'nao sei','ceilandia norte', '2');
+
+INSERT INTO pessoa (nome, ativo, numero, cep, cidade, estado, logradouro, bairro, complemento)
+  values ('Fabiana', 1, '34', '7226001', 'taguatingua', 'df', 'nao sei','pistao', 's');
+
+INSERT INTO pessoa (nome, ativo, numero, cep, cidade, estado, logradouro, bairro, complemento)
+  values ('Carlos', 0, '35', '7226002', 'gama', 'df', 'nao sei', 'gama', '2');
+
+INSERT INTO pessoa (nome, ativo, numero, cep, cidade, estado, logradouro, bairro, complemento)
+  values ('Maria', 1, '36', '7226003', 'brasilia', 'df', 'nao sei','asa norte', '12');
+
+
