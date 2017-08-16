b785859132a803249de7c62ebcdabb002c3ae8c2
vitormarcal <1Qazxsw2>
2017-08-16 12:38:01 -0300

    create: pessoa

---
+++ src/main/java/com/vitormarcal/marcalmoney/api/model/Endereco.java
@@ -0,0 +1,4 @@
+package com.vitormarcal.marcalmoney.api.model;
+
+public class Endereco {
+}
---
+++ src/main/java/com/vitormarcal/marcalmoney/api/model/Pessoa.java
@@ -0,0 +1,53 @@
+package com.vitormarcal.marcalmoney.api.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @NotNull
+    @Size(min = 5, max = 50)
+    private String nome;
+
+    @NotNull
+    private boolean ativo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pessoa pessoa = (Pessoa) o;
+
+        return codigo.equals(pessoa.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo.hashCode();
+    }
+}
