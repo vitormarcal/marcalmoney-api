0a818d8987ecdcc524138a404a53cdb0fdb9b626
Vítor Marçal <git@vitormarcal.com.br>
2017-08-17 13:12:21 -0300

    adicionado serviço de remoçao de pessoas

--- src/main/java/com/vitormarcal/marcalmoney/api/resource/PessoaResource.java
+++ src/main/java/com/vitormarcal/marcalmoney/api/resource/PessoaResource.java
@@ -3,6 +3,7 @@ package com.vitormarcal.marcalmoney.api.resource;
 import com.vitormarcal.marcalmoney.api.model.Pessoa;
 import com.vitormarcal.marcalmoney.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -37,5 +38,11 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo){
 		Pessoa pessoa = pessoaRepository.findOne(codigo);
 		return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo) {
+		pessoaRepository.delete(codigo);
 	}
 }
